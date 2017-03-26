6cce1cfe615acd28dc78db4bf9ab309ec10bcb86
Gabriella Gazdecki <gazdgabr@umich.edu>
2017-03-25 23:56:57 -0400

    everything but 10 done; too tired to do it today

--- SI206W17_HW6.py
+++ SI206W17_HW6.py
@@ -158,30 +158,39 @@ long_names_list = list(long_names)
 ## [PROBLEM 9]
 print("\n\n***** Problem 9 *****")
 
-# Use a list comprehension to generate a LIST of just the names of those Student instances whose name is longer than their seniority (i.e., ["Albert", "Mai", "Dinesh", "Euijin"]). Assign it to a variable called names_with_not_too_much_seniority.
+# Use a list comprehension to generate a LIST of just the names of those Student instances whose name is longer than their seniority 
+# (i.e., ["Albert", "Mai", "Dinesh", "Euijin"]). Assign it to a variable called names_with_not_too_much_seniority.
 
 ## Note that you can use another list you have already created for this problem.
 
-
+names_with_not_too_much_seniority = [name[0] for name in student_tups_list if len(name[0]) > name[1]]
 
 
 ## [PROBLEM 10]
 print("\n\n***** Problem 10 *****")
 
-## Define a function called readfiles, which accepts a list of filenames as input and yields each line in each of the file with that name, assuming those files exist in the same directory as this program.
+## Define a function called readfiles, which accepts a list of filenames as input and yields each line in each of the file with that name, assuming those
+# files exist in the same directory as this program.
 
-## Define a generator called len_check which accepts a generator of file lines and returns a generator object of all the lines it's accepted whose length is longer than 40 characters.
+## Define a generator called len_check which accepts a generator of file lines and returns a generator object of all the lines it's accepted whose length
+# is longer than 40 characters.
 
-## Finally, write a function called main_filterer that accepts a list of filenames (strings), and returns a generator of all the lines in those files that are longer than 40 characters. The function should invoke the other function and generator, readfiles and len_check.
+## Finally, write a function called main_filterer that accepts a list of filenames (strings), and returns a generator of all the lines in those files that
+# are longer than 40 characters. The function should invoke the other function and generator, readfiles and len_check.
 
-## There is a test for this but an even more fun test is to uncomment the code below which invokes the main_filterer function and prints each line from the generator without blank lines in between (that's what the comma is doing).
+## There is a test for this but an even more fun test is to uncomment the code below which invokes the main_filterer function and prints each line from the
+# generator without blank lines in between (that's what the comma is doing).
 
 ## The examples here http://anandology.com/python-practice-book/iterators.html in your reading may be very helpful!
 
-## We have provided files samplehw6_1.txt and samplehw6_2.txt for your use for this problem, which hopefully you have downloaded, so you can test with those file names! The test below also relies upon these files. Of course, you could also create other files for testing.
+## We have provided files samplehw6_1.txt and samplehw6_2.txt for your use for this problem, which hopefully you have downloaded, so you can test with those
+# file names! The test below also relies upon these files. Of course, you could also create other files for testing.
 
 # Define readfiles (make sure to close the file reference in the right place)
-
+def readfiles(filenames):
+    for file in filenames:
+        for line in file:
+            yield line
 
 # Define len_check
 
